a6d0017e46c55c7485222fbae4c6599615c64e79
CoolGuyMK <coolguymk1@gmail.com>
2021-03-20 20:51:37 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.400c4329776d2f89a5c841bc9b936b9f.js"
+  "/precache-manifest.6224d54a26616180f074d1e9c233b379.js"
 );
 
 self.addEventListener('message', (event) => {
