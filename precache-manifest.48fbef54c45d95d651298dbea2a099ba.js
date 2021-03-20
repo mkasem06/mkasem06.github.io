2449af2875c52bc88702f5465c174701df401c8e
CoolGuyMK <coolguymk1@gmail.com>
2021-03-20 19:24:11 +0200

    Updates

---
+++ precache-manifest.48fbef54c45d95d651298dbea2a099ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae31f86aca54fd919124a5e302ef10a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7ae4aad2d06049420cb",
+    "url": "/static/css/main.7ed7da15.chunk.css"
+  },
+  {
+    "revision": "97cf846ddb3d0c3defb7",
+    "url": "/static/js/2.462210cd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.462210cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7ae4aad2d06049420cb",
+    "url": "/static/js/main.5d65ac68.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "89e5db7ff8e3a8162824a8e189475f82",
+    "url": "/static/media/CoolGuyMK.89e5db7f.png"
+  },
+  {
+    "revision": "d784fa8b6d98d27699781bd9a7cf19f0",
+    "url": "/static/media/resume.d784fa8b.pdf"
+  }
+]);
